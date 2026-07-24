f8a9215a198e917044a49cb98958d22a56665271
songmumu <983826781@qq.com>
2026-07-24 11:54:51 +0800

    chore: remove temp debug files

--- backend/check_717.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-db = sqlite3.connect(r'C:\Users\admin\.qclaw\workspace-v733kxt9elzfv7u1\trading-system\backend\trading.db')
-cur = db.execute("SELECT code, date, close_price FROM price_history WHERE date = '2026-07-17'")
-for r in cur:
-    print(r)
